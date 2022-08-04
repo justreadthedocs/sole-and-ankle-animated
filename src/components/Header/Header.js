953d5419433ab908c7f99b8c3ad32d935e96ede2
Aidar <110410228+justreadthedocs@users.noreply.github.com>
2022-08-04 19:52:45 +0300

    2: Add aura effect to navigation links
    Similar to the one from:
    https://tympanus.net/Development/CreativeLinkEffects/

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,16 +20,16 @@ const Header = () => {
           <Logo />
         </LogoWrapper>
         <DesktopNav>
-          <NavLinkWithFlipUpEffect href='/sale'>Sale</NavLinkWithFlipUpEffect>
-          <NavLinkWithFlipUpEffect href='/new'>
+          <NavLinkWithAuraEffect href='/sale'>Sale</NavLinkWithAuraEffect>
+          <NavLinkWithAuraEffect href='/new'>
             New&nbsp;Releases
-          </NavLinkWithFlipUpEffect>
-          <NavLinkWithFlipUpEffect href='/men'>Men</NavLinkWithFlipUpEffect>
-          <NavLinkWithFlipUpEffect href='/women'>Women</NavLinkWithFlipUpEffect>
-          <NavLinkWithFlipUpEffect href='/kids'>Kids</NavLinkWithFlipUpEffect>
-          <NavLinkWithFlipUpEffect href='/collections'>
+          </NavLinkWithAuraEffect>
+          <NavLinkWithAuraEffect href='/men'>Men</NavLinkWithAuraEffect>
+          <NavLinkWithAuraEffect href='/women'>Women</NavLinkWithAuraEffect>
+          <NavLinkWithAuraEffect href='/kids'>Kids</NavLinkWithAuraEffect>
+          <NavLinkWithAuraEffect href='/collections'>
             Collections
-          </NavLinkWithFlipUpEffect>
+          </NavLinkWithAuraEffect>
         </DesktopNav>
         <MobileActions>
           <ShoppingBagButton>
@@ -142,6 +142,68 @@ const FlippableNavLinkFixedText = styled.span`
     ${NavLink}:hover &, ${NavLink}:focus-visible & {
       opacity: 0;
       transition-delay: 0ms;
+    }
+  }
+`;
+
+const NavLinkWithAuraEffect = styled(NavLink)`
+  // TODO: make it so that the radius is proportional to the element's width,
+  // rather than being fixed.
+  // TODO: make it work on keyboard focus — there are some weird ghost outlines...
+  @media (prefers-reduced-motion: no-preference) {
+    position: relative;
+    isolation: isolate;
+
+    --circle1-radius: 16px;
+    --circle1-scale: 1;
+    --circle1-opacity: 0;
+    --circle1-color: var(--color-gray-100);
+    --circle1-transition: transform 200ms ease-in, opacity 400ms ease-out;
+
+    --circle2-radius: 12px;
+    --circle2-scale: 1;
+    --circle2-opacity: 0;
+    --circle2-color: var(--color-gray-100);
+    --circle2-transition: transform 300ms ease-out, opacity 100ms ease-out;
+
+    &:hover {
+      --circle1-scale: 1.2;
+      --circle1-opacity: 1;
+      --circle2-scale: 2;
+      --circle2-opacity: 1;
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: calc(50% - var(--circle1-radius));
+      left: calc(50% - var(--circle1-radius));
+      width: calc(2 * var(--circle1-radius));
+      height: calc(2 * var(--circle1-radius));
+      border-radius: 50%;
+      background: transparent;
+      border: 3px solid var(--circle1-color);
+      transform: scale(var(--circle1-scale));
+      opacity: var(--circle1-opacity);
+      transition: var(--circle1-transition);
+      z-index: -1;
+    }
+
+    &:after {
+      content: '';
+      outline: none;
+      position: absolute;
+      top: calc(50% - var(--circle2-radius));
+      left: calc(50% - var(--circle2-radius));
+      width: calc(2 * var(--circle2-radius));
+      height: calc(2 * var(--circle2-radius));
+      border-radius: 50%;
+      background: transparent;
+      border: 1px solid var(--circle2-color);
+      transform: scale(var(--circle2-scale));
+      opacity: var(--circle2-opacity);
+      transition: var(--circle2-transition);
+      z-index: -1;
     }
   }
 `;
